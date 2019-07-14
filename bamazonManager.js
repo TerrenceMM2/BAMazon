0ba8aab24a7d10d43471af62f7d36068bc2205fd
TerrenceMM2 <terrencemm2@gmail.com>
2019-07-14 15:46:45 -0500

    adds add to inventory functionality

--- bamazonManager.js
+++ bamazonManager.js
@@ -20,7 +20,6 @@ inquirer.prompt([{
         "Add New Product"
     ]
 }]).then(answers => {
-    console.log(answers);
     switch (answers.adminAction) {
         case "View Products for Sale":
             getProducts();
@@ -29,10 +28,11 @@ inquirer.prompt([{
             lowInventory();
             break;
         case "Add to Inventory":
+            addInventory();
             break;
         case "Add New Product":
             break;
-    }
+    };
 });
 
 function getProducts() {
@@ -86,4 +86,62 @@ function lowInventory() {
 
     connection.end();
 
+};
+
+function addInventory() {
+    connection.connect();
+
+    connection.query('SELECT * FROM products', function (error, results) {
+        if (error) throw error;
+        var results = results;
+        var data = [];
+        for (var i = 0; i < results.length; i++) {
+            data.push({
+                product_id: results[i].id,
+                product_name: results[i].product_name,
+                product_department: results[i].department_name,
+                product_price: results[i].price,
+                product_quantity: results[i].stock_quantity
+            });
+        };
+        var columns = columnify(data, {
+            columnSplitter: " | "
+        });
+        console.log(columns);
+        specifyProduct();
+    });
+};
+
+function specifyProduct() {
+    inquirer.prompt([{
+            type: "input",
+            name: "productId",
+            message: "Please enter the Product ID."
+        },
+        {
+            type: "input",
+            name: "quantity",
+            message: "How many units would you like to add?"
+        }
+    ]).then(answers => {
+        updateInventory(answers.productId, answers.quantity);
+    });
+};
+
+function updateInventory(num1, num2) {
+    var num1 = parseInt(num1);
+    var num2 = parseInt(num2);
+
+    connection.query('SELECT * FROM products WHERE id = ?', [num1], function (error, results) {
+        console.log("THERE HAS BEEN AN ERROR")
+        if (error) throw error;
+        var results = results[0];
+        var currentStock = results.stock_quantity;
+        var updateStock = currentStock + num2;
+        connection.query("UPDATE products SET stock_quantity = ? WHERE id = ?", [updateStock, num1], function (error) {
+            if (error) throw error;
+        });
+        connection.end();
+        console.log("\nInventory updated placed!\n");
+    });
 };
