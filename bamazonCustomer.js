6bd137af354e2b556e172fe93412a6e7bbf7c032
TerrenceMM2 <terrencemm2@gmail.com>
2019-07-13 21:49:20 -0500

    adds functionality to place order and update stock

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -34,21 +34,45 @@ function getProducts() {
         productSearch();
     });
 
-    connection.end();
+    // connection.end();
 };
 
 function productSearch() {
     inquirer.prompt([{
-        type: "input",
-        name: "productId",
-        message: "Please input the ID of the product you would like to purchase."
-    },
-    {
-        type: "input",
-        name: "quantity",
-        message: "How many units would you like to purchase?"
-    }
-]).then(answers => {
-    console.log("I would like to purchase " + answers.quantity + " " + answers.productId + ".");
-})
-}
+            type: "input",
+            name: "productId",
+            message: "Please input the ID of the product you would like to purchase."
+        },
+        {
+            type: "input",
+            name: "quantity",
+            message: "How many units would you like to purchase?"
+        }
+    ]).then(answers => {
+        makePurchase(answers.productId, answers.quantity);
+    });
+};
+
+function makePurchase(num1, num2) {
+    var num1 = parseInt(num1);
+    var num2 = parseInt(num2);
+
+    connection.query('SELECT * FROM products WHERE id = ?', [num1], function (error, results) {
+        if (error) throw error;
+        var results = results[0];
+        var currentStock = results.stock_quantity;
+        if (currentStock < num2) {
+            console.log("Insufficient quantity!");
+            console.log(">>> Canceling order");
+            connection.end();
+        } else {
+            var updateStock = currentStock - num2;
+            connection.query("UPDATE products SET stock_quantity = ? WHERE id = ?", [updateStock, num1]);
+            console.log("Order placed!");
+            var orderTotal = results.price * num2;
+            console.log("Order total = $" + orderTotal);
+            console.log("Thank you for your business.");
+            connection.end();
+        };
+    });
+};
